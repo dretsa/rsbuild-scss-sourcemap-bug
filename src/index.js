61f0cbd2576969f0e0c5747e854835d04f702cc3
Andrey Tsarev <andrey.tsarev@fpsoftware.io>
2025-01-28 12:06:34 +0200

    Test another SCSS file import in JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './index.scss';
+import './another.scss';
 
 document.querySelector('#root').innerHTML = `
-<div class="alert alert-danger m-5" role="alert">
+<div class="alert alert-danger m-5 another-file" role="alert">
   All sourcemaps are coming from index.scss
 </div>
 `;
